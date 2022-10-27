4f6df10bd07b21a9a29ab6c2194dc8811111d535
Kumaguro3 <lily.sergent@gmail.com>
2022-10-27 22:01:05 +0900

    Try commit to fix issue

--- components/TopNav.vue
+++ components/TopNav.vue
@@ -2,18 +2,15 @@
     <div class="p-4 border-b flex justify-between">
         <div class="font-semibold text-xl text-gray-800">
             <NuxtLink class="align-middle" to="/">
-                <img class="inline align-middle pr-2"
-                    src="../assets/images/logo-32-px-color.svg" />Find a Doc
+                <img class="inline align-middle pr-2" src="../assets/images/logo-32-px-color.svg" />Find a Doc
             </NuxtLink>
         </div>
         <div>
             <NuxtLink to="/about">
-                About
+                About Us
             </NuxtLink>
             <div class="cursor-pointer md:hidden">
-                <img @click="toggleMenu"
-                    src="../assets/images/hamburgerMenu.svg"
-                    alt="hamburger menu" />
+                <img @click="toggleMenu" src="../assets/images/hamburgerMenu.svg" alt="hamburger menu" />
                 <div v-if="showMenu">
                     <div>
                         <span class="block">Home</span>
